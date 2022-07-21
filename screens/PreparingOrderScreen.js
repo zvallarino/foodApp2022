9b88b586bdd08098938b72802cdbb96e4373f1aa
zachary vallarino <zvallarino@gmail.com>
2022-07-20 18:31:35 -0700

    zv123

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,9 +1,17 @@
 import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import * as Animatable from 'react-native-animatable'
 import * as Progress from 'react-native-progress'
+import { useNavigation } from '@react-navigation/native'
 
 const PreparingOrderScreen = () => {
+
+  const navigation = useNavigation()
+
+  useEffect(()=>{
+    setTimeout(()=>{navigation.navigate("Delivery")},4000)
+  },[])
+
   return (
     <SafeAreaView className='flex-1 bg-[#00CCBB] justify-center items-center'>
       <Animatable.Image
